4b86864a61ddd675d238bc6e0b486a388cac1f8c
nonoSword08 <chensijie08@gmail.com>
2020-07-04 22:12:10 +0800

    refactor wallet with error

--- 06pointer/wallet_test.go
+++ 06pointer/wallet_test.go
@@ -11,7 +11,14 @@ func TestWallet(t *testing.T) {
         if got != want {
             t.Errorf("got %s want %s", got, want)
         }
-    }
+	}
+	
+	assertError := func(t *testing.T, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
 
     t.Run("Deposit", func(t *testing.T) {
         wallet := Wallet{}
@@ -23,6 +30,16 @@ func TestWallet(t *testing.T) {
         wallet := Wallet{balance: Bitcoin(20)}
         wallet.Withdraw(Bitcoin(10))
         assertBalance(t, wallet, Bitcoin(10))
-    })
+	})
+	
+	t.Run("Withdraw insufficent funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+		
+		assertError(t, err)
+	})
 
 }
