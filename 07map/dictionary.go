141101271f4190f4bea86b6ea740a5ca29e451d0
nonoSword08 <chensijie08@gmail.com>
2020-07-05 19:25:06 +0800

    refactor map Add()

--- 07map/dictionary.go
+++ 07map/dictionary.go
@@ -4,11 +4,17 @@ import "errors"
 
 type Dictionary map[string]string
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
 	definiton, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return definiton, nil
+}
+
+func (d Dictionary) Add(word, definiton string) {
+	d[word] = definiton
 }
--- 07map/dictionary_test.go
+++ 07map/dictionary_test.go
@@ -14,13 +14,13 @@ func TestSearch(t *testing.T) {
 
 	t.Run("unknow word", func(t *testing.T) {
 		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
+		want := ErrNotFound
 
 		if err == nil {
 			t.Fatal("execpet to get an error")
 		}
 
-		assertStrings(t, err.Error(), want)
+		assertError(t, err, want)
 	})
 
 	got, _ := dictionary.Search("test")
@@ -30,10 +30,40 @@ func TestSearch(t *testing.T) {
 
 }
 
+func TestAdd(t *testing.T) {
+	dictionnary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+	
+	dictionnary.Add(word, definition)
+
+	assertDefinition(t, dictionnary, word, definition)
+}
+
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+    t.Helper()
+
+    got, err := dictionary.Search(word)
+    if err != nil {
+        t.Fatal("should find added word:", err)
+    }
+
+    if definition != got {
+        t.Errorf("got %q want %q", got, definition)
+    }
 }
