9134a85972c6219d9dba8eb87fa9d631c449f0bb
nonoSword08 <chensijie08@gmail.com>
2020-07-05 12:42:59 +0800

    refactor wallet

--- 06pointer/wallet_test.go
+++ 06pointer/wallet_test.go
@@ -4,29 +4,13 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
-    assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
-        t.Helper()
-        got := wallet.Balance()
-
-        if got != want {
-            t.Errorf("got %s want %s", got, want)
-        }
-	}
-	
-	assertError := func(t *testing.T, err error) {
-		t.Helper()
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
-	}
-
     t.Run("Deposit", func(t *testing.T) {
         wallet := Wallet{}
         wallet.Deposit(Bitcoin(10))
         assertBalance(t, wallet, Bitcoin(10))
     })
 
-    t.Run("Withdraw", func(t *testing.T) {
+    t.Run("Withdraw with funds", func(t *testing.T) {
         wallet := Wallet{balance: Bitcoin(20)}
         wallet.Withdraw(Bitcoin(10))
         assertBalance(t, wallet, Bitcoin(10))
@@ -39,7 +23,28 @@ func TestWallet(t *testing.T) {
 
 		assertBalance(t, wallet, startingBalance)
 		
-		assertError(t, err)
+		assertError(t, err, ErrInsufficientFunds)
 	})
 
+}
+
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+    t.Helper()
+    got := wallet.Balance()
+
+    if got != want {
+        t.Errorf("got %q want %q", got, want)
+    }
+}
+
+func assertError(t *testing.T, got error, want error) {
+    t.Helper()
+    if got == nil {
+        t.Fatal("didn't get an error but wanted one")
+    }
+
+    if got != want {
+        t.Errorf("got %q, want %q", got, want)
+    }
 }
