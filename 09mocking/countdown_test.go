0db0d31d5718830121d1a8b186067de203f12a48
nonoSword08 <chensijie08@gmail.com>
2020-07-07 22:33:19 +0800

    test print and sleep sequence

--- 09mocking/countdown_test.go
+++ 09mocking/countdown_test.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
+
+const write = "write"
+const sleep = "sleep"
+
+type CountdownOperationsSpy struct {
+    Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+    s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+    s.Calls = append(s.Calls, write)
+    return
+}
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
@@ -24,4 +41,24 @@ Go!`
 	if spySleeper.Calls != 4 {
 		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
 	}
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationsSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+	
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+	
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
