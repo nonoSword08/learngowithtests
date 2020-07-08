1b9228a98a43d88d01ce6f5804f2058c03c5266c
nonoSword08 <chensijie08@gmail.com>
2020-07-08 17:52:55 +0800

    refactor select

--- 11select/racer_test.go
+++ 11select/racer_test.go
@@ -9,28 +9,30 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
 
+	defer slowServer.Close()
+	defer fastServer.Close()
 
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		w.WriteHeader(http.StatusOK)
-	}))
-	
 	slowURL := slowServer.URL
-	fasrURl := fastServer.URL
+	fastURL := fastServer.URL
 
 	fmt.Print(slowURL)
 
-	want := fasrURl
-	got := Racer(slowURL, fasrURl)
+	want := fastURL
+	got := Racer(slowURL, fastURL)
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 
-	slowServer.Close()
-	fastServer.Close()
+	
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
