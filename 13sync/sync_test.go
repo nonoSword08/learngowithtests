072f4017749be00fb17093eb32d33c5af0dd1435
nonoSword08 <chensijie08@gmail.com>
2020-07-12 16:54:24 +0800

    refactor sync with safely concurrent

--- 13sync/sync_test.go
+++ 13sync/sync_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("increamting the conuter 3 times leaves it at 3", func(t *testing.T) {
@@ -9,12 +12,31 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 
+		assertConuter(t, counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantCount)
+
+		for i := 0; i < wantCount; i++ {
+			go func(wg *sync.WaitGroup) {
+				counter.Inc()
+				wg.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertConuter(t, counter, wantCount)
 	})
 }
 
 func assertConuter(t *testing.T, got Counter, want int) {
 	t.Helper()
-	if got.Value() != 3 {
-		t.Errorf("got %d want %d", got.Value(), 3)
+	if got.Value() != want {
+		t.Errorf("got %d want %d", got.Value(), want)
 	}
 }
