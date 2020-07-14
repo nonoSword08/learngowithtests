92d9fe92f67b79cc88ebbd76b9374e72ddc5d5c8
nonoSword08 <chensijie08@gmail.com>
2020-07-14 22:26:26 +0800

    refactor server with struct and interface

--- 14http_server/server.go
+++ 14http_server/server.go
@@ -8,25 +8,21 @@ import (
 )
 
 func main() {
-	handler := http.HandlerFunc(PlayerServer)
-	if err := http.ListenAndServe(":5000", handler); err != nil {
+	server := &PlayerServer{}
+	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
 }
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, GetPlayerScore(player))
+type PlayerStore interface {
+	GetPlayerScore(name string) int
 }
 
-func GetPlayerScore(name string) string {
-	if name == "Pepper" {
-		return "20"
-	}
+type PlayerServer struct {
+	store PlayerStore
+}
 
-	if name == "Floyd" {
-		return "10"
-	}
-
-	return ""
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
