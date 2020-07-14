4add643a5e64f0a6d56d49b3d873c4fbf45d8ef9
nonoSword08 <chensijie08@gmail.com>
2020-07-14 22:00:09 +0800

    refactor http_server with smallest step

--- 14http_server/server.go
+++ 14http_server/server.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	if err := http.ListenAndServe(":5000", handler); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
+}
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+
+	if name == "Floyd" {
+		return "10"
+	}
+
+	return ""
 }
