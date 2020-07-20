acf175492815d87829b0d3533e488a2a64d54c21
nonoSword08 <chensijie08@gmail.com>
2020-07-20 14:12:26 +0800

    refactor io test "get player score"

--- 16IO/file_system_store.go
+++ 16IO/file_system_store.go
@@ -8,8 +8,21 @@ type FileSystemPlayerStore struct {
 	database io.ReadSeeker
 }
 
+// 解析数据库中的存储的json数据并以Player列表的形式返回所有存储的数据
 func (f *FileSystemPlayerStore) GetLeague() []Player {
-	f.database.Seek(0, 0)  // 每次读取时seek到文件开头处
+	f.database.Seek(0, 0) // 每次读取时seek到文件开头处
 	league, _ := NewLeague(f.database)
 	return league
+}
+
+// 返回某位玩家的数据
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	var wins int
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+	return wins
 }
