7e8ce72928f9adb5a424bbb416ed8bb05ad2522d
nonoSword08 <chensijie08@gmail.com>
2020-07-22 18:31:28 +0800

    refactor io store wins for existing players

--- 16IO/file_system_store.go
+++ 16IO/file_system_store.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 )
 
@@ -9,7 +10,7 @@ type FileSystemPlayerStore struct {
 }
 
 // 解析数据库中的存储的json数据并以Player列表的形式返回所有存储的数据
-func (f *FileSystemPlayerStore) GetLeague() []Player {
+func (f *FileSystemPlayerStore) GetLeague() League {
 	f.database.Seek(0, 0) // 每次读取时seek到文件开头处
 	league, _ := NewLeague(f.database)
 	return league
@@ -17,12 +18,34 @@ func (f *FileSystemPlayerStore) GetLeague() []Player {
 
 // 返回某位玩家的数据
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	var wins int
-	for _, player := range f.GetLeague() {
-		if player.Name == name {
-			wins = player.Wins
-			break
+	player := f.GetLeague().Find(name)
+	if player != nil {
+		return player.Wins
+	}
+	return 0
+}
+
+// 记录某位玩家得分
+func (f *FileSystemPlayerStore) RecordWin(name string) {
+	league := f.GetLeague()
+	player := league.Find(name)
+
+	if player != nil {
+		player.Wins++
+	}
+
+	f.database.Seek(0, 0)
+	json.NewEncoder(f.database).Encode(league)
+}
+
+// 得分列表包装类
+type League []Player
+
+func (l League) Find(name string) *Player {
+	for i, p := range l {
+		if p.Name==name {
+			return &l[i]
 		}
 	}
-	return wins
+	return nil
 }
--- 16IO/test_util.go
+++ 16IO/test_util.go
@@ -13,22 +13,22 @@ import (
 
 // 创建一个临时文件，返回文件对象，用于存储数据
 func createTempFile(t *testing.T, initialData string) (io.ReadWriteSeeker, func()) {
-    t.Helper()
+	t.Helper()
 
-    tmpfile, err := ioutil.TempFile("", "db")
+	tmpfile, err := ioutil.TempFile("", "db")
 
-    if err != nil {
-        t.Fatalf("could not create temp file %v", err)
-    }
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
 
-    tmpfile.Write([]byte(initialData))
+	tmpfile.Write([]byte(initialData))
 
-    removeFile := func() {
-    	tmpfile.Close()
-        os.Remove(tmpfile.Name())
-    }
+	removeFile := func() {
+		// tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
 
-    return tmpfile, removeFile
+	return tmpfile, removeFile
 }
 
 
