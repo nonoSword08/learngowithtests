f03f8679285b2760927e3e3f02b01ef651d8b938
nonoSword08 <chensijie08@gmail.com>
2020-07-24 23:01:54 +0800

    refactor io implement store wins for new players

--- 16IO/file_system_store_test.go
+++ 16IO/file_system_store_test.go
@@ -57,4 +57,19 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertScoreEquals(t, got, want)
 	})
+
+	t.Run("store wins for new players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+	
+		store := FileSystemPlayerStore{database}
+	
+		store.RecordWin("Pepper")
+	
+		got := store.GetPlayerScore("Pepper")
+		want := 1
+		assertScoreEquals(t, got, want)
+	})
 }
